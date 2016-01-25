1a262c460c7d1a482d410c1d333fc75e08c7e404
Haley Bristol and Oskar Radon <hebristol@gmail.com>
2016-01-25 10:02:03 -0800

    add JQuery show places

--- Desktop/places/js/scripts.js
+++ Desktop/places/js/scripts.js
@@ -3,11 +3,27 @@ function Place (city, country, landmark) {
   this.country = country;
   this.landmark = landmark;
 }
+
+Place.prototype.fullPlace = function() {
+  return this.city + ", " + this.country;
+
+}
 $(function (){
   $("form#place").submit(function(event){
     event.preventDefault;
     var city = $("input#new-city").val();
     var country = $("input#new-country").val();
     var landmark = $("input#new-landmark").val();
+    var newPlace = new Place(city, country);
+
+  $("ul#places").append("<li><span class='fullPlace'>" + newPlace.fullPlace() + "</span></li>");
+
+    $(".contact").last().click(function() {
+      $("#show-places").show();
+      $("#show-places h2").text(newPlace.fullPlace());
+      $(".city").text(newPlace.city);
+      $(".country").text(newPlace.country);
+      $(".landmark").text(newPlace.landmark);
+    });
   });
 });
