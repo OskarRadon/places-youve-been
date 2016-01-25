56fd2649b7be2eee9c5dfb6a559d2e17755c41fc
Haley Bristol and Oskar Radon <hebristol@gmail.com>
2016-01-25 09:03:00 -0800

    add first passing spec

---
+++ Desktop/places/js/scripts.js
@@ -0,0 +1,5 @@
+function Place (city, country, landmark) {
+  this.city = city;
+  this.country = country;
+  this.landmark = landmark;
+}
---
+++ Desktop/places/spec/specs.js
@@ -0,0 +1,9 @@
+describe ("Place", function(){
+  it("will contain info for place", function(){
+    var testPlace = new Place("Paris", "France", "Eiffel Tower")
+    expect(testPlace.city).to.equal("Paris");
+    expect(testPlace.country).to.equal("France");
+    expect(testPlace.landmark).to.equal("Eiffel Tower");
+  });
+
+});
