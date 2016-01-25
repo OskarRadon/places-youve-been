bfccd7a42234a55ebe9a5b28fa0a8421d35d2121
Haley Bristol and Oskar Radon <oskarradonkimball@gmail.com>
2016-01-25 10:08:56 -0800

    Fix form selector in jQuery

--- Desktop/places/js/scripts.js
+++ Desktop/places/js/scripts.js
@@ -1,4 +1,4 @@
-function Place (city, country, landmark) {
+function Place (city,country,landmark) {
   this.city = city;
   this.country = country;
   this.landmark = landmark;
@@ -6,17 +6,18 @@ function Place (city, country, landmark) {
 
 Place.prototype.fullPlace = function() {
   return this.city + ", " + this.country;
-
 }
-$(function (){
-  $("form#place").submit(function(event){
-    event.preventDefault;
+
+$(document).ready(function() {
+  $("form#new-place").submit(function(event) {
+    event.preventDefault();
+
     var city = $("input#new-city").val();
     var country = $("input#new-country").val();
     var landmark = $("input#new-landmark").val();
     var newPlace = new Place(city, country);
 
-  $("ul#places").append("<li><span class='fullPlace'>" + newPlace.fullPlace() + "</span></li>");
+    $("ul#places").append("<li><span class='fullPlace'>" + newPlace.fullPlace() + "</span></li>");
 
     $(".contact").last().click(function() {
       $("#show-places").show();
